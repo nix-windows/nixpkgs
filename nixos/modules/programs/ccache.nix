17ef3191aa8e2bf4d341a0913578c63834b29d70
Volth <volth@webmaster.ms>
2017-08-08 08:05:08 +0000

    separate host and target options for nixops (and maybe cross-compilation)

--- nixos/modules/programs/ccache.nix
+++ nixos/modules/programs/ccache.nix
@@ -5,11 +5,17 @@ let
   cfg = config.programs.ccache;
 in {
   options.programs.ccache = {
+    # host configuration
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+    };
     cacheDir = mkOption {
       type = types.path;
       description = "CCache directory";
       default = "/var/cache/ccache";
     };
+    # target configuration
     packageNames = mkOption {
       type = types.listOf types.str;
       description = "Nix top-level packages to be compiled using CCache";
@@ -18,21 +24,29 @@ in {
     };
   };
 
-  config = mkIf (cfg.packageNames != []) {
-    nixpkgs.overlays = [
-      (self: super: genAttrs cfg.packageNames (pn: super.${pn}.override { stdenv = builtins.trace "with ccache: ${pn}" self.ccacheStdenv; }))
+  config = mkMerge [
+    # host configuration
+    (mkIf cfg.enable {
+      nixpkgs.overlays = [
+        (self: super: {
+          ccacheWrapper = super.ccacheWrapper.override {
+            extraConfig = ''
+              export CCACHE_COMPRESS=1
+              export CCACHE_DIR=${cfg.cacheDir}
+              export CCACHE_UMASK=007
+            '';
+          };
+        })
+      ];
 
-      (self: super: {
-        ccacheWrapper = super.ccacheWrapper.override {
-          extraConfig = ''
-            export CCACHE_COMPRESS=1
-            export CCACHE_DIR=${cfg.cacheDir}
-            export CCACHE_UMASK=007
-          '';
-        };
-      })
-    ];
+      systemd.tmpfiles.rules = [ "d ${cfg.cacheDir} 0770 root nixbld -" ];
+    })
 
-    systemd.tmpfiles.rules = [ "d ${cfg.cacheDir} 0770 root nixbld -" ];
-  };
+    # target configuration
+    (mkIf (cfg.packageNames != []) {
+      nixpkgs.overlays = [
+        (self: super: genAttrs cfg.packageNames (pn: super.${pn}.override { stdenv = builtins.trace "with ccache: ${pn}" self.ccacheStdenv; }))
+      ];
+    })
+  ];
 }
