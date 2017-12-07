38a23664d652c8d455319f81940a96cf54203fd0
John Doe <John Doe john@example.com>
2017-12-07 16:43:15 +0000

    xx

--- pkgs/applications/virtualization/virtualbox/default.nix
+++ pkgs/applications/virtualization/virtualbox/default.nix
@@ -199,6 +199,6 @@ in stdenv.mkDerivation {
     description = "PC emulator";
     homepage = http://www.virtualbox.org/;
     maintainers = [ lib.maintainers.sander ];
-    platforms = [ "aarch64-linux" "x86_64-linux" "i686-linux" ];
+    platforms = [ "x86_64-linux" "i686-linux" ];
   };
 }
