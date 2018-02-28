006cf7607a1916dc1f5548513867db27109e8169
volth <volth@volth.com>
2018-02-28 22:30:19 +0000

    perl package Array-Compare: 2.11 -> v3.0.0

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -377,10 +377,10 @@ let self = _self // overrides; _self = with self; {
   };
 
   ArrayCompare = buildPerlPackage rec {
-    name = "Array-Compare-2.11";
+    name = "Array-Compare-v3.0.0";
     src = fetchurl {
-      url = "mirror://cpan/authors/id/D/DA/DAVECROSS/${name}.tar.gz";
-      sha256 = "0f1mg2lpr5jzxy1hciww7vlp4r602vfwpzsqmhkgv1i107pmiwcs";
+      url = "mirror://cpan/authors/id/D/DA/DAVECROSS/Array-Compare-v3.0.0.tar.gz";
+      sha256 = "1j1lrpgwh78vs6wqnzlvvfvqc4x6nwi5yvv86i5bfd79h9j4vk9j";
     };
 
     buildInputs = [ TestNoWarnings Moo TypeTiny ];
