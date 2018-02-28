60c51130053f9b35bb27891984d12e721a5a89d5
volth <volth@volth.com>
2018-02-28 22:31:43 +0000

    perl package Test-CleanNamespaces: 0.16 -> 0.22

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -12413,10 +12413,10 @@ let self = _self // overrides; _self = with self; {
   };
 
   TestCleanNamespaces = buildPerlPackage {
-    name = "Test-CleanNamespaces-0.16";
+    name = "Test-CleanNamespaces-0.22";
     src = fetchurl {
-      url = mirror://cpan/authors/id/E/ET/ETHER/Test-CleanNamespaces-0.16.tar.gz;
-      sha256 = "9779378394b9be32cf04129fafe2d40d74f6f200f593f1494998bd128a6ed9fa";
+      url = "mirror://cpan/authors/id/E/ET/ETHER/Test-CleanNamespaces-0.22.tar.gz";
+      sha256 = "1jma95agqqy7iwdcl6jbg1waqz7mjqng4l046lpknhfxjhcj4al6";
     };
     buildInputs = [ ModuleRuntime TestDeep TestRequires TestTester TestWarnings if_ ];
     propagatedBuildInputs = [ FileFindRule FileFindRulePerl ModuleRuntime PackageStash SubExporter SubIdentify namespaceclean ];
