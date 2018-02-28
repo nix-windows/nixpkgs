ff9135fad7ad1d7a97ea4d2c0125e792a9fbd836
volth <volth@volth.com>
2018-02-28 22:31:23 +0000

    perl package MooseX-Types-Structured: 0.30 -> 0.36

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -9215,10 +9215,10 @@ let self = _self // overrides; _self = with self; {
   };
 
   MooseXTypesStructured = buildPerlPackage {
-    name = "MooseX-Types-Structured-0.30";
+    name = "MooseX-Types-Structured-0.36";
     src = fetchurl {
-      url = mirror://cpan/authors/id/E/ET/ETHER/MooseX-Types-Structured-0.30.tar.gz;
-      sha256 = "0svfgbyzwdipywh7bfp954hncm8ihfr8xpppcyy59wr1inx2f55c";
+      url = "mirror://cpan/authors/id/E/ET/ETHER/MooseX-Types-Structured-0.36.tar.gz";
+      sha256 = "0mrxc00sid7526c6brrnjr6288468sszic3wazij71v3z59bdka3";
     };
     buildInputs = [ DateTime MooseXTypesDateTime TestFatal ];
     propagatedBuildInputs = [ DevelPartialDump Moose MooseXTypes SubExporter ];
