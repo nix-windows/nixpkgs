47043fd016167e18855c806861c6bfbb881d6852
volth <volth@webmaster.ms>
2018-06-08 21:51:10 +0000

    perlPackages.DBDSQLite: 1.55_07 -> 1.58

--- pkgs/development/perl-modules/DBD-SQLite/default.nix
+++ pkgs/development/perl-modules/DBD-SQLite/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, buildPerlPackage, DBI, sqlite }:
 
 buildPerlPackage rec {
-  name = "DBD-SQLite-1.57_01";
+  name = "DBD-SQLite-1.58";
 
   src = fetchurl {
-    url = mirror://cpan/authors/id/I/IS/ISHIGAKI/DBD-SQLite-1.57_01.tar.gz;
-    sha256 = "1111cwlqkf4837y8mf8sag2cma82r059iy0869y2bz4bz88v2zzs";
+    url = mirror://cpan/authors/id/I/IS/ISHIGAKI/DBD-SQLite-1.58.tar.gz;
+    sha256 = "0fqx386jgs9mmrknr7smmzapf07izgivza7x08lfm39ks2cxs83i";
   };
 
   propagatedBuildInputs = [ DBI ];
@@ -22,10 +22,6 @@ buildPerlPackage rec {
     # Get rid of a pointless copy of the SQLite sources.
     rm -rf $out/lib/perl5/site_perl/*/*/auto/share
   '';
-
-  # Disabled because the tests can randomly fail due to timeouts
-  # (e.g. "database is locked(5) at dbdimp.c line 402 at t/07busy.t").
-  #doCheck = false;
 
   meta = with stdenv.lib; {
     description = "Self Contained SQLite RDBMS in a DBI Driver";
