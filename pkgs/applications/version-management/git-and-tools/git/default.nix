1142609fa9aa4e1fc15f00b6cf80e871442f8270
Your Name <you@example.com>
2018-12-21 23:56:21 +0000

    git minimal: native windows build

--- pkgs/applications/version-management/git-and-tools/git/default.nix
+++ pkgs/applications/version-management/git-and-tools/git/default.nix
@@ -1,5 +1,6 @@
 { fetchurl, stdenv, buildPackages
 , curl, openssl, zlib, expat, perl, python, gettext, cpio
+, writeTextFile
 , gnugrep, gnused, gawk, coreutils # needed at runtime by git-filter-branch etc
 , openssh, pcre2
 , asciidoc, texinfo, xmlto, docbook2x, docbook_xsl, docbook_xml_dtd_45
@@ -21,10 +22,6 @@ assert svnSupport -> perlSupport;
 
 let
   version = "2.19.1";
-  svn = subversionClient.override { perlBindings = perlSupport; };
-in
-
-stdenv.mkDerivation {
   name = "git-${version}";
 
   src = fetchurl {
@@ -32,6 +29,166 @@ stdenv.mkDerivation {
     sha256 = "1dfv43lmdnxz42504jc89sihbv1d4d6kgqcz3c5ji140kfm5cl1l";
   };
 
+  svn = subversionClient.override { perlBindings = perlSupport; };
+in
+
+if stdenv.hostPlatform.isMicrosoft then
+
+#let
+#  gnumake = fetchurl/*Boot*/ {
+#    url = https://raw.githubusercontent.com/mbuilov/gnumake-windows/master/gnumake-4.2.1-x64.exe;
+#    sha256 = "0fly79df9330im0r4xr25d5yi46kr23p5s9mybjfz28v930n2zx5";
+#  };
+#in
+stdenv.mkDerivation {
+  inherit name version src;
+
+# src = ../../../../../../t/git-2.19.1;
+
+  nativeBuildInputs = [ perl ];
+
+  dontConfigure = true;
+
+  buildPhase = ''
+    my %defines = (
+      'WIN32'                       => "",
+      'NO_MBSUPPORT'                => "",
+      'NO_NSEC'                     => "",
+      'NO_SYS_POLL_H'               => "",
+      'NO_POLL'                     => "",
+      'NO_ICONV'                    => "",
+      'USE_WIN32_MMAP'              => "",
+      'NO_GETTEXT'                  => "",
+      'NO_LIBGEN_H'                 => "",
+      'GAWK'                        => "",
+      'MAX_PATH'                    => '260',
+      'O_ACCMODE'                   => '(O_RDONLY|O_WRONLY|O_RDWR)',
+      'sigset_t'                    => 'int',
+      'bool'                        => 'int',
+      'false'                       => '0',
+      'true'                        => '1',
+      'ftello'                      => 'ftell',
+      'PROTECT_NTFS_DEFAULT'        => '1',
+      'OBJECT_CREATION_MODE'        => '1',
+      'NO_POSIX_GOODIES'            => "",
+      'NO_ST_BLOCKS_IN_STRUCT_STAT' => "",
+      'NO_PREAD'                    => "",
+      'NO_STRLCPY'                  => "",
+      'NO_MEMMEM'                   => "",
+      'NO_MKDTEMP'                  => "",
+      'NO_SETENV'                   => "",
+      'NO_STRCASESTR'               => "",
+      'NO_STRTOUMAX'                => "",
+      'NOGDI'                       => "",
+      'UNRELIABLE_FSTAT'            => "",
+      'RUNTIME_PREFIX'              => "",
+      'SHA1_BLK'                    => "",
+      'HAVE_ALLOCA_H'               => "",
+      'HAVE_STRING_H'               => "",
+    # 'HAVE_WPGMPTR'                => "",
+      'BINDIR'                      => '"bin"',
+      'GIT_EXEC_PATH'               => '"libexec/git-core"',
+      'FALLBACK_RUNTIME_PREFIX'     => '"C:/git"',
+      'PAGER_ENV'                   => '"LESS=FRX"',
+      'GIT_HOST_CPU'                => '"x86_64"',
+      'STRIP_EXTENSION'             => '".exe"',
+      'ETC_GITCONFIG'               => '"etc/gitconfig"',
+      'ETC_GITATTRIBUTES'           => '"etc/gitattributes"',
+      'GIT_HTML_PATH'               => '"share/doc/git-doc"',
+      'GIT_INFO_PATH'               => '"share/info"',
+      'GIT_MAN_PATH'                => '"share/man"',
+      'GIT_VERSION'                 => '"2.19.1.MSVC"',
+      'GIT_USER_AGENT'              => '"git/2.19.1.MSVC"',
+      'GIT_BUILT_FROM_COMMIT'       => '"-"',
+      'NTDDI_VERSION'               => '0');
+
+      $ENV{INCLUDE}=".;compat;compat/poll;compat/regex;compat/vcbuild;compat/vcbuild/include;compat/win32;${openssl}/include;${zlib}/include;${curl}/include";
+      my @libgit_srcs = (
+        "block-sha1/sha1.c",
+        "compat/basename.c",
+        "compat/inet_ntop.c",
+        "compat/inet_pton.c",
+        "compat/memmem.c",
+        "compat/mkdtemp.c",
+        "compat/msvc.c",
+        "compat/obstack.c",
+        "compat/poll/poll.c",
+        "compat/pread.c",
+        "compat/qsort_s.c",
+        "compat/regex/regex.c",
+        "compat/setenv.c",
+        "compat/snprintf.c",
+        "compat/strcasestr.c",
+        "compat/strlcpy.c",
+        "compat/strtoimax.c",
+        "compat/strtoumax.c",
+        "compat/terminal.c",
+        "compat/win32mmap.c",
+        "compat/win32/dirent.c",
+        "compat/win32/pthread.c",
+        "compat/win32/syslog.c",
+        "compat/winansi.c",
+        glob("ewah/*.c"),
+        glob("negotiator/*.c"),
+        glob("refs/*.c"),
+        grep { $_ !~ /(imap-send|credential-cache--daemon|check-racy|shell|credential-store|http-push|sh-i18n--envsubst|fast-import|common-main|sha1dc_git|http-walker|http|daemon|unix-socket|credential-cache|http-fetch|http-backend|remote-curl|remote-testsvn|git)\.c/ } glob('*.c'));
+      my @xdiff_srcs = glob("xdiff/*.c");
+      my @git_srcs = (glob("builtin/*.c"), "common-main.c", "git.c");
+      my @git_remote_http_srcs = ("common-main.c", "http-walker.c", "http.c", "remote-curl.c");
+
+      # `./command-list.h` needs `bash` to generate (todo: do it once `stdenvMinGW` will work)
+      copy('${./command-list-2.19.1.h}', './command-list.h') or die;
+
+      # VS2017's msvcrt.lib has no __wgetmainargs
+      open(my $fh, ">>compat/mingw.c");
+      print $fh 'int __wgetmainargs(int *argc, wchar_t ***argv, wchar_t ***env, int glob, _startupinfo *si) {';
+      print $fh '  int (*pfn)(int *, wchar_t ***, wchar_t ***, int, _startupinfo *);';
+      print $fh '  pfn = GetProcAddress(LoadLibrary("msvcrt"), "__wgetmainargs");';
+      print $fh '  assert(pfn);';
+      print $fh '  return pfn(argc, argv, env, glob, si);';
+      print $fh '}';
+      close($fh);
+
+      mkdir('xdiffobjs') or die $!;
+      my @cmd = ("cl.exe", '-c', '-MD', '-Zi', '-GL', '-Fo:xdiffobjs/',  (map { '"' . ("-D$_=$defines{$_}" =~ s/"/"""/gr) . '"' } keys %defines), @xdiff_srcs);
+      print(join(' ', @cmd), "\n");
+      system(@cmd);
+      system("lib.exe",  "/out:xdiff.lib",  '/LTCG', glob('xdiffobjs/*.obj'));
+
+      mkdir('libgitobjs') or die $!;
+      my @cmd = ("cl.exe", '-c', '-MD', '-Zi', '-GL', '-Fo:libgitobjs/', (map { '"' . ("-D$_=$defines{$_}" =~ s/"/"""/gr) . '"' } keys %defines), @libgit_srcs);
+      print(join(' ', @cmd), "\n");
+      system(@cmd);
+      system("lib.exe",  "/out:libgit.lib", '/LTCG', glob('libgitobjs/*.obj'));
+
+      mkdir('gitobjs') or die $!;
+      my @cmd = ("cl.exe", '-c', '-MD', '-Zi', '-GL', '-Fo:gitobjs/',    (map { '"' . ("-D$_=$defines{$_}" =~ s/"/"""/gr) . '"' } keys %defines), @git_srcs);
+      print(join(' ', @cmd), "\n");
+      system(@cmd);
+      system("link.exe", "/out:git.exe",              '/DEBUG', '/LTCG', '/SUBSYSTEM:CONSOLE', glob('gitobjs/*.obj'),   "libgit.lib", "xdiff.lib", 'advapi32.lib', 'ws2_32.lib', 'user32.lib', "${zlib}/lib/zlib.lib");
+
+      mkdir('gitrhobjs') or die $!;
+      my @cmd = ("cl.exe", '-c', '-MD', '-Zi', '-GL', '-Fo:gitrhobjs/',  (map { '"' . ("-D$_=$defines{$_}" =~ s/"/"""/gr) . '"' } keys %defines), @git_remote_http_srcs);
+      print(join(' ', @cmd), "\n");
+      system(@cmd);
+      system("link.exe", "/out:git-remote-http.exe",  '/DEBUG', '/LTCG', '/SUBSYSTEM:CONSOLE', glob('gitrhobjs/*.obj'), "libgit.lib", "xdiff.lib", 'advapi32.lib', 'ws2_32.lib', 'user32.lib', "${zlib}/lib/zlib.lib", "${curl}/lib/libcurl.lib");
+      system("link.exe", "/out:git-remote-https.exe", '/DEBUG', '/LTCG', '/SUBSYSTEM:CONSOLE', glob('gitrhobjs/*.obj'), "libgit.lib", "xdiff.lib", 'advapi32.lib', 'ws2_32.lib', 'user32.lib', "${zlib}/lib/zlib.lib", "${curl}/lib/libcurl.lib");
+  '';
+  installPhase = ''
+    mkdir($ENV{out}) or die;
+    mkdir("$ENV{out}/bin") or die;
+    copy("git.exe",                 "$ENV{out}/bin/") or die;
+    copy("git.pdb",                 "$ENV{out}/bin/") or die;
+    copy("git-remote-http.exe",     "$ENV{out}/bin/") or die;
+    copy("git-remote-https.exe",    "$ENV{out}/bin/") or die;
+    copy("${curl}/bin/libcurl.dll", "$ENV{out}/bin/") or die;
+  '';
+}
+
+else
+
+stdenv.mkDerivation {
+  inherit name version src;
   outputs = [ "out" ] ++ stdenv.lib.optional perlSupport "gitweb";
 
   hardeningDisable = [ "format" ];
@@ -237,7 +394,7 @@ stdenv.mkDerivation {
     # enable git-credential-osxkeychain by default if darwin
     cat > $out/etc/gitconfig << EOF
 [credential]
-	helper = osxkeychain
+    helper = osxkeychain
 EOF
   '';
 
