087b083262f354daee6b5e57c19636d8b10c3df3
volth <volth@volth.com>
2018-12-22 18:45:10 +0000

    gn: build on windows

--- pkgs/development/tools/build-managers/gn/default.nix
+++ pkgs/development/tools/build-managers/gn/default.nix
@@ -1,9 +1,12 @@
 { stdenv, lib, fetchgit, fetchzip, fetchpatch, darwin, writeText
-, git, ninja, python }:
+, git, ninja, python2 }:
 
 let
   rev = "106b823805adcc043b2bfe5bc21d58f160a28a7b";
-  sha256 = "1a5s6i07s8l4f1bakh3fyaym00xz7zgd49sp6awm10xb7yjh95ba";
+  sha256 = if stdenv.hostPlatform.isMicrosoft then
+             "1gscv6lpaj6xwvc4jhqk6nh4kxjsbmaj4d11bahnpf8zpifqr29c" /* without executable bit */
+           else
+             "1a5s6i07s8l4f1bakh3fyaym00xz7zgd49sp6awm10xb7yjh95ba";
 
   shortRev = builtins.substring 0 7 rev;
   lastCommitPosition = writeText "last_commit_position.h" ''
@@ -25,13 +28,13 @@ stdenv.mkDerivation rec {
     inherit rev sha256;
   };
 
-  postPatch = ''
+  postPatch = lib.optionalString stdenv.isDarwin ''
     # FIXME Needed with old Apple SDKs
     substituteInPlace base/mac/foundation_util.mm \
       --replace "NSArray<NSString*>*" "NSArray*"
   '';
 
-  nativeBuildInputs = [ ninja python git ];
+  nativeBuildInputs = [ ninja python2 ] ++ lib.optional (!stdenv.hostPlatform.isMicrosoft) git;
   buildInputs = lib.optionals stdenv.isDarwin (with darwin; with apple_sdk.frameworks; [
     libobjc
     cctools
@@ -42,13 +45,20 @@ stdenv.mkDerivation rec {
     AppKit
   ]);
 
-  buildPhase = ''
+  buildPhase = if stdenv.hostPlatform.isMicrosoft then ''
+    system('python build/gen.py --no-sysroot --no-last-commit-position') == 0 or die;
+    copy('${lastCommitPosition}', 'out/last_commit_position.h') or die;
+    system("ninja -j $ENV{NIX_BUILD_CORES} -C out gn.exe") == 0 or die;
+  '' else ''
     python build/gen.py --no-sysroot --no-last-commit-position
     ln -s ${lastCommitPosition} out/last_commit_position.h
     ninja -j $NIX_BUILD_CORES -C out gn
   '';
 
-  installPhase = ''
+  installPhase = if stdenv.hostPlatform.isMicrosoft then ''
+    mkpath("$ENV{out}/bin") or die $!;
+    copy("out/gn.exe", "$ENV{out}/bin/") or die $!;
+  '' else ''
     install -vD out/gn "$out/bin/gn"
   '';
 
@@ -56,7 +66,7 @@ stdenv.mkDerivation rec {
     description = "A meta-build system that generates NinjaBuild files";
     homepage = https://gn.googlesource.com/gn;
     license = licenses.bsd3;
-    platforms = platforms.unix;
+    platforms = platforms.unix ++ platforms.windows;
     maintainers = with maintainers; [ stesie matthewbauer ];
   };
 }
