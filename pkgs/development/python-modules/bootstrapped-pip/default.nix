1b68360e7c9ae062fe5ab2c6a26ced2a9e3ef6ed
volth <volth@volth.com>
2018-12-23 20:25:23 +0000

    pythonPackages.bootstrapped-pip: windows build

--- pkgs/development/python-modules/bootstrapped-pip/default.nix
+++ pkgs/development/python-modules/bootstrapped-pip/default.nix
@@ -1,4 +1,41 @@
-{ stdenv, python, fetchPypi, makeWrapper, unzip }:
+{ stdenv, python, fetchPypi, makeWrapper, unzip, setuptools }:
+
+if stdenv.hostPlatform.isMicrosoft then
+
+stdenv.mkDerivation rec {
+  pname = "pip";
+  version = "18.1";
+  name = "${python.libPrefix}-bootstrapped-${pname}-${version}";
+
+  src = fetchPypi {
+    inherit pname version;
+    extension = "tar.gz";
+    sha256 = "188fclay154s520n43s7cxxlhdaiysvxf19zk8vr1xbyjyyr58n0";
+  };
+
+# buildInputs = [ python ];
+
+  installPhase = ''
+    make_path "$ENV{out}/${python.sitePackages}";
+    $ENV{PYTHONPATH}="$ENV{out}/${python.sitePackages};${setuptools}/${python.sitePackages};$ENV{PYTHONPATH}";
+    system("${python.interpreter} setup.py install --prefix=$ENV{out}");
+
+    # poor man's wrapPythonPrograms (until it is clear what wrapPythonPrograms should do on Windows)
+    my @extrapaths = ("$ENV{out}/${python.sitePackages}", '${setuptools}/${python.sitePackages}');
+    my $newline = "import site;import functools;functools.reduce(lambda k, p: site.addsitedir(p, k), ['".
+                  join("','", map { s|\\|/|rg } @extrapaths).
+                  "'], site._init_pathinfo());";
+    for my $filename (glob("$ENV{out}/Scripts/*.py")) {
+      open(my $fh, $filename);
+      my @newlines = map { /^# EASY-INSTALL-ENTRY-SCRIPT/ ?  "$_\n$newline\n\n" : $_ } <$fh>;
+      open(my $fh2, ">$filename");
+      print $fh2 @newlines;
+      close($fh2);
+    }
+  '';
+}
+
+else
 
 let
   wheel_source = fetchPypi {
@@ -9,9 +46,9 @@ let
   };
   setuptools_source = fetchPypi {
     pname = "setuptools";
-    version = "40.4.3";
+    version = "40.6.3";
     format = "wheel";
-    sha256 = "1zzryv1fqx4nk7hzv4afxpl8k7f60lc0qkhxlc8sqjs4igakfhff";
+    sha256 = "02yx9g0k48k20kfamwp8bjv8w05mramhl0gd65xcyd1ghfdcxhg2";
   };
 
 in stdenv.mkDerivation rec {
