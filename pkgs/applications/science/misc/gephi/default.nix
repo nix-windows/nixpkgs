2be2552e4b9780b53355b325414fc42bdc185b51
volth <volth@volth.com>
2019-01-19 13:46:01 +0000

    gephi: windows build

--- pkgs/applications/science/misc/gephi/default.nix
+++ pkgs/applications/science/misc/gephi/default.nix
@@ -7,7 +7,10 @@ let
     owner = "gephi";
     repo = "gephi";
     rev = "v${version}";
-    sha256 = "0kqp2nvnsb55j1axb6hk0mlw5alyaiyb70z0mdybhpqqxyw2da2r";
+    sha256 = if stdenv.hostPlatform.isMicrosoft then
+               "1x01qikc968yql2mrl8cdqd51gsa7g7iwyc76bpn6iaasimv088z" # ignoring executable bit
+             else
+               "0kqp2nvnsb55j1axb6hk0mlw5alyaiyb70z0mdybhpqqxyw2da2r";
   };
 
   # perform fake build to make a fixed-output derivation out of the files downloaded from maven central (120MB)
@@ -15,13 +18,21 @@ let
     name = "gephi-${version}-deps";
     inherit src;
     buildInputs = [ jdk maven ];
-    buildPhase = ''
+    buildPhase = if stdenv.isShellPerl then ''
+      while(system("mvn package -Dmaven.repo.local=$ENV{out}/.m2 -Dmaven.wagon.rto=5000") == 1) {
+        print("timeout, restart maven to continue downloading\n");
+      }
+    '' else ''
       while mvn package -Dmaven.repo.local=$out/.m2 -Dmaven.wagon.rto=5000; [ $? = 1 ]; do
         echo "timeout, restart maven to continue downloading"
       done
     '';
     # keep only *.{pom,jar,sha1,nbm} and delete all ephemeral files with lastModified timestamps inside
-    installPhase = ''find $out/.m2 -type f -regex '.+\(\.lastUpdated\|resolver-status\.properties\|_remote\.repositories\)' -delete'';
+    installPhase = if stdenv.isShellPerl then ''
+      findL { unlinkL($_) if -f $_ && /\.lastUpdated$|resolver-status\.properties$|_remote\.repositories$/; } "$ENV{out}/.m2"
+    '' else ''
+      find $out/.m2 -type f -regex '.+\(\.lastUpdated\|resolver-status\.properties\|_remote\.repositories\)' -delete
+    '';
     outputHashAlgo = "sha256";
     outputHashMode = "recursive";
     outputHash = "1p7yf97dn0nvr005cbs6vdk3i341s8fya4kfccj8qqad2qgxflif";
@@ -34,12 +45,19 @@ stdenv.mkDerivation rec {
 
   buildInputs = [ jdk maven ];
 
-  buildPhase = ''
+  buildPhase = if stdenv.isShellPerl then ''
+    system("mvn package --offline -Dmaven.repo.local=${deps}/.m2") == 0 or die;
+  '' else ''
     # 'maven.repo.local' must be writable so copy it out of nix store
     mvn package --offline -Dmaven.repo.local=$(cp -dpR ${deps}/.m2 ./ && chmod +w -R .m2 && pwd)/.m2
   '';
 
-  installPhase = ''
+  installPhase = if stdenv.isShellPerl then ''
+    dircopy('modules/application/target/gephi', $ENV{out});
+    findL { unlinkL($_) if /\.lastModified$/ } $ENV{out};
+    changeFile { $_."\njdkhome=${jdk}" } "$ENV{out}/etc/gephi.conf";
+    ${ if stdenv.is64bit then ''copyL("$ENV{out}/bin/gephi64.exe", "$ENV{out}/bin/gephi.exe");'' else ''unlinkL("$ENV{out}/bin/gephi64.exe");'' }
+  '' else ''
     cp -r modules/application/target/gephi $out
 
     # remove garbage
@@ -54,11 +72,13 @@ stdenv.mkDerivation rec {
     echo "jdkhome=${jdk}" >> $out/etc/gephi.conf
   '';
 
+  passthru.deps = deps;
+
   meta = with stdenv.lib; {
     description = "A platform for visualizing and manipulating large graphs";
     homepage = https://gephi.org;
     license = licenses.gpl3;
     maintainers = [ maintainers.taeer ];
-    platforms = [ "x86_64-linux" ];
+    platforms = [ "x86_64-linux" "x86_64-windows" "i686-windows" ];
   };
 }
