c900ddfb6a8fdf459d785722dee4742775fd6dd8
volth <volth@volth.com>
2019-01-19 13:17:24 +0000

    jdk8: 192->201

--- pkgs/development/compilers/openjdk/windows/8.nix
+++ pkgs/development/compilers/openjdk/windows/8.nix
@@ -7,12 +7,19 @@ let
   };
 
   jdk = stdenv.mkDerivation {
-    name = "zulu1.8.0_192-8.33.0.1";
+    name = "zulu1.8.0_201-8.34.0.1";
 
-    src = fetchurl {
-      url = "https://cdn.azul.com/zulu/bin/zulu8.33.0.1-jdk8.0.192-win_x64.zip";
-      sha256 = "08pn5v6adv2bx6v5mnhhg8yr09167d88dizcxy1jai54v7kk0wq4";
-    };
+    src =
+      if stdenv.is64bit then
+        fetchurl {
+          url = "https://cdn.azul.com/zulu/bin/zulu8.34.0.1-ca-jdk8.0.201-win_x64.zip";
+          sha256 = "1fx3p7l9lh5ir6v8jpv2arg5i6y9r84z329y5ky8zjydy3iqb786";
+        }
+      else
+        fetchurl {
+          url = "https://cdn.azul.com/zulu/bin/zulu8.34.0.1-ca-jdk8.0.201-win_i686.zip";
+          sha256 = "14m21n3n5a899yws3b0y583hljc9jnqnq1vz58dq132d279v9iw2";
+        };
 
     installPhase = ''
       dircopy('.',                                  $ENV{out}                                        ) or die $!;
