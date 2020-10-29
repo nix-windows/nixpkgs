522aa944c4a457fdf0b63226f51bf554413eaa8f
volth <volth@volth.com>
2020-10-28 19:50:54 -0700

    perl: make in buildable with MinGW

--- pkgs/development/interpreters/perl/default.nix
+++ pkgs/development/interpreters/perl/default.nix
@@ -1,20 +1,7 @@
-{ lib, stdenv, fetchurlBoot, buildPackages
-, enableThreading ? stdenv ? glibc, makeWrapper
+{ lib, stdenv, fetchurlBoot
 }:
 
 with lib;
-
-# We can only compile perl with threading on platforms where we have a
-# real glibc in the stdenv.
-#
-# Instead of silently building an unthreaded perl if this is not the
-# case, we force callers to disableThreading explicitly, therefore
-# documenting the platforms where the perl is not threaded.
-#
-# In the case of stdenv linux boot stage1 it's not possible to use
-# threading because of the simpleness of the bootstrap glibc, so we
-# use enableThreading = false there.
-assert enableThreading -> (stdenv ? glibc);
 
 let
   meta = {
@@ -25,200 +12,63 @@ let
     platforms = platforms.all;
   };
 
-  common = { version, sha256 }: if stdenv.hostPlatform.isMicrosoft then stdenv.mkDerivation (rec {
-    inherit version meta;
+  common = { version, sha256 }:
+    let
+      name = "perl-${version}";
+      src = fetchurlBoot {
+        url = "mirror://cpan/src/5.0/${name}.tar.gz";
+        inherit sha256;
+      };
+    in if stdenv.hostPlatform.isMicrosoft && stdenv.cc.isMSVC then
+      stdenv.mkDerivation rec {
+        inherit name version src meta;
+        disallowedReferences = [ stdenv.cc /*stdenv.cc.msvc*/ ]; # <- FIXME, the must not me reference to huge stdenv.cc.msvc (they are mostly in .pdb and .lib files)
+        dontConfigure = true;
+        # PROCESSOR_ARCHITECTURE needs for cross-compilation; should it be buildPlatform's or hostPlatform's ?
+        buildPhase = ''
+          chdir('win32');
+          system("nmake install INST_TOP=$ENV{out} CCTYPE=${if lib.versionAtLeast stdenv.cc.msvc.version "8" && lib.versionOlder stdenv.cc.msvc.version "9" then
+                                                              "MSVC80"
+                                                            else if lib.versionAtLeast stdenv.cc.msvc.version "14.10" && lib.versionOlder stdenv.cc.msvc.version "14.20" then
+                                                              "MSVC141"
+                                                            else if lib.versionAtLeast stdenv.cc.msvc.version "14.20" && lib.versionOlder stdenv.cc.msvc.version "14.30" then
+                                                              "MSVC142"
+                                                            else
+                                                              throw "???"} ${if stdenv.is64bit then "WIN64=define PROCESSOR_ARCHITECTURE=AMD64" else "WIN64=undef PROCESSOR_ARCHITECTURE=X86"} BUILD_STATIC=define");
+        '';
+        # todo: common hook looking for missing dlls and copying/hardlinking them into %out%/bin/
+        fixupPhase = lib.optionalString (lib.versionAtLeast stdenv.cc.msvc.version "14.10" && lib.versionOlder stdenv.cc.msvc.version "14.20") ''
+          copyL('${stdenv.cc.redist}/${if stdenv.is64bit then "x64" else "x86"}/Microsoft.VC141.CRT/vcruntime140.dll'   => "$ENV{out}/bin/vcruntime140.dll"  );
+        '' + lib.optionalString (lib.versionAtLeast stdenv.cc.msvc.version "14.20" && lib.versionOlder stdenv.cc.msvc.version "14.30") ''
+          copyL('${stdenv.cc.redist}/${if stdenv.is64bit then "x64" else "x86"}/Microsoft.VC142.CRT/vcruntime140.dll'   => "$ENV{out}/bin/vcruntime140.dll"  );
+          copyL('${stdenv.cc.redist}/${if stdenv.is64bit then "x64" else "x86"}/Microsoft.VC142.CRT/vcruntime140_1.dll' => "$ENV{out}/bin/vcruntime140_1.dll");
+        '';
+        setupHook = ./setup-hook.pl;
+        passthru.libPrefix = "lib";
+#       doCheck = false; # some tests fail, expensive
+      }
+    else if stdenv.hostPlatform.isMicrosoft && stdenv.cc.isGNU then
+      stdenv.mkDerivation rec {
+        inherit name version src meta;
+        disallowedReferences = [ stdenv.cc stdenv.cc.cc ];
+        dontConfigure = true;
+        patches = [ ../../../stdenv/windows/perl-on-gcc10.patch ];
+        buildPhase = ''
+          chdir('win32');
+          system("make.exe -j$ENV{NIX_BUILD_CORES} -f GNUmakefile install PLMAKE=make.exe INST_TOP=$ENV{out} CCTYPE=GCC ${if stdenv.is64bit then "WIN64=define GCCTARGET=x86_64-w64-mingw32" else "WIN64=undef GCCTARGET=i686-w64-mingw32"}");
+        '';
+        # todo: common hook looking for missing dlls and copying/hardlinking them into %out%/bin/
+        fixupPhase = ''
+          copyL("${stdenv.cc.redist}/mingw${if stdenv.is64bit then "64" else "32"}/bin/libgcc_s_dw2-1.dll"  => "$ENV{out}/bin/libgcc_s_dw2-1.dll" ); # TODO: hardlink
+          copyL("${stdenv.cc.redist}/mingw${if stdenv.is64bit then "64" else "32"}/bin/libstdc++-6.dll"     => "$ENV{out}/bin/libstdc++-6.dll"    ); # TODO: hardlink
+          copyL("${stdenv.cc.redist}/mingw${if stdenv.is64bit then "64" else "32"}/bin/libwinpthread-1.dll" => "$ENV{out}/bin/libwinpthread-1.dll"); # TODO: hardlink
+        '';
+        setupHook = ./setup-hook.pl;
+        passthru.libPrefix = "lib";
+      }
+    else
+      throw "???";
 
-    name = "perl-${version}";
-
-    src = fetchurlBoot {
-      url = "mirror://cpan/src/5.0/${name}.tar.gz";
-      inherit sha256;
-    };
-
-#   disallowedReferences = [ stdenv.cc ];
-
-#   unpackPhase = ''
-#       for my $k (sort (keys %ENV)) { print("ENV $k = '$ENV{$k}'\n"); }
-#       #print("PATH=$ENV{PATH}\n");
-#       exit(1);
-#   '';
-
-    dontConfigure = true;
-    # PROCESSOR_ARCHITECTURE needs for cross-compilation; should it be buildPlatform's or hostPlatform's ?
-    buildPhase = ''
-      chdir('win32');
-
-      system("nmake install INST_TOP=$ENV{out} CCTYPE=${if stdenv.cc.isMSVC && lib.versionAtLeast stdenv.cc.msvc.version "8" && lib.versionOlder stdenv.cc.msvc.version "9" then
-                                                          "MSVC80"
-                                                        else if stdenv.cc.isMSVC && lib.versionAtLeast stdenv.cc.msvc.version "14.10" && lib.versionOlder stdenv.cc.msvc.version "14.20" then
-                                                          "MSVC141"
-                                                        else if stdenv.cc.isMSVC && lib.versionAtLeast stdenv.cc.msvc.version "14.20" && lib.versionOlder stdenv.cc.msvc.version "14.30" then
-                                                          "MSVC142"
-                                                        else
-                                                          throw "???"} ${if stdenv.is64bit then "WIN64=define PROCESSOR_ARCHITECTURE=AMD64" else "WIN64=undef PROCESSOR_ARCHITECTURE=X86"} BUILD_STATIC=define");
-    '';
-
-    # todo: common hook looking for missing dlls and copying/hardlinking them into %out%/bin/
-    fixupPhase = lib.optionalString (stdenv.cc.isMSVC && lib.versionAtLeast stdenv.cc.msvc.version "14.10" && lib.versionOlder stdenv.cc.msvc.version "14.20") ''
-      copyL('${stdenv.cc.redist}/${if stdenv.is64bit then "x64" else "x86"}/Microsoft.VC141.CRT/vcruntime140.dll'   => "$ENV{out}/bin/vcruntime140.dll"  );
-    '' + lib.optionalString (stdenv.cc.isMSVC && lib.versionAtLeast stdenv.cc.msvc.version "14.20" && lib.versionOlder stdenv.cc.msvc.version "14.30") ''
-      copyL('${stdenv.cc.redist}/${if stdenv.is64bit then "x64" else "x86"}/Microsoft.VC142.CRT/vcruntime140.dll'   => "$ENV{out}/bin/vcruntime140.dll"  );
-      copyL('${stdenv.cc.redist}/${if stdenv.is64bit then "x64" else "x86"}/Microsoft.VC142.CRT/vcruntime140_1.dll' => "$ENV{out}/bin/vcruntime140_1.dll");
-    '';
-
-    setupHook = ./setup-hook.pl;
-
-    passthru.libPrefix = "lib";
-
-#   doCheck = false; # some tests fail, expensive
-  })
-  else let
-    libc = if stdenv.cc.libc or null != null then stdenv.cc.libc else "/usr";
-    libcInc = lib.getDev libc;
-    libcLib = lib.getLib libc;
-    crossCompiling = stdenv.buildPlatform != stdenv.hostPlatform;
-  in stdenv.mkDerivation (rec {
-    inherit version;
-
-    name = "perl-${version}";
-
-    src = fetchurlBoot {
-      url = "mirror://cpan/src/5.0/${name}.tar.gz";
-      inherit sha256;
-    };
-
-    # TODO: Add a "dev" output containing the header files.
-    outputs = [ "out" "man" "devdoc" ] ++
-      stdenv.lib.optional crossCompiling "dev";
-    setOutputFlags = false;
-
-    disallowedReferences = [ stdenv.cc ];
-
-    patches =
-      [
-        # Do not look in /usr etc. for dependencies.
-        ./no-sys-dirs-5.26.patch
-        # Fix parallel building: https://rt.perl.org/Public/Bug/Display.html?id=132360
-        (fetchurlBoot {
-          url = "https://rt.perl.org/Public/Ticket/Attachment/1502646/807252/0001-Fix-missing-build-dependency-for-pods.patch";
-          sha256 = "1bb4mldfp8kq1scv480wm64n2jdsqa3ar46cjp1mjpby8h5dr2r0";
-        })
-      ]
-      ++ optional stdenv.isSunOS ./ld-shared.patch
-      ++ optionals stdenv.isDarwin [ ./cpp-precomp.patch ./sw_vers.patch ]
-      ++ optional crossCompiling ./MakeMaker-cross.patch;
-
-    postPatch = ''
-      pwd="$(type -P pwd)"
-      substituteInPlace dist/PathTools/Cwd.pm \
-        --replace "/bin/pwd" "$pwd"
-    '' + stdenv.lib.optionalString crossCompiling ''
-      substituteInPlace cnf/configure_tool.sh --replace "cc -E -P" "cc -E"
-    '';
-
-    # Build a thread-safe Perl with a dynamic libperls.o.  We need the
-    # "installstyle" option to ensure that modules are put under
-    # $out/lib/perl5 - this is the general default, but because $out
-    # contains the string "perl", Configure would select $out/lib.
-    # Miniperl needs -lm. perl needs -lrt.
-    configureFlags =
-      (if crossCompiling
-       then [ "-Dlibpth=\"\"" "-Dglibpth=\"\"" ]
-       else [ "-de" "-Dcc=cc" ])
-      ++ [
-        "-Uinstallusrbinperl"
-        "-Dinstallstyle=lib/perl5"
-        "-Duseshrplib"
-        "-Dlocincpth=${libcInc}/include"
-        "-Dloclibpth=${libcLib}/lib"
-      ]
-      ++ optionals ((builtins.match ''5\.[0-9]*[13579]\..+'' version) != null) [ "-Dusedevel" "-Uversiononly" ]
-      ++ optional stdenv.isSunOS "-Dcc=gcc"
-      ++ optional enableThreading "-Dusethreads";
-
-    configureScript = stdenv.lib.optionalString (!crossCompiling) "${stdenv.shell} ./Configure";
-
-    dontAddPrefix = !crossCompiling;
-
-    enableParallelBuilding = !crossCompiling;
-
-    preConfigure = optionalString (!crossCompiling) ''
-        configureFlags="$configureFlags -Dprefix=$out -Dman1dir=$out/share/man/man1 -Dman3dir=$out/share/man/man3"
-      '' + optionalString (stdenv.isAarch32 || stdenv.isMips) ''
-        configureFlagsArray=(-Dldflags="-lm -lrt")
-      '' + optionalString stdenv.isDarwin ''
-        substituteInPlace hints/darwin.sh --replace "env MACOSX_DEPLOYMENT_TARGET=10.3" ""
-      '' + optionalString (!enableThreading) ''
-        # We need to do this because the bootstrap doesn't have a static libpthread
-        sed -i 's,\(libswanted.*\)pthread,\1,g' Configure
-      '';
-
-    setupHook = ./setup-hook.sh;
-
-    passthru.libPrefix = "lib/perl5/site_perl";
-
-    doCheck = false; # some tests fail, expensive
-
-    # TODO: it seems like absolute paths to some coreutils is required.
-    postInstall =
-      ''
-        # Remove dependency between "out" and "man" outputs.
-        rm "$out"/lib/perl5/*/*/.packlist
-
-        # Remove dependencies on glibc and gcc
-        sed "/ *libpth =>/c    libpth => ' '," \
-          -i "$out"/lib/perl5/*/*/Config.pm
-        # TODO: removing those paths would be cleaner than overwriting with nonsense.
-        substituteInPlace "$out"/lib/perl5/*/*/Config_heavy.pl \
-          --replace "${libcInc}" /no-such-path \
-          --replace "${
-              if stdenv.cc.cc or null != null then stdenv.cc.cc else "/no-such-path"
-            }" /no-such-path \
-          --replace "${stdenv.cc}" /no-such-path \
-          --replace "$man" /no-such-path
-      '' + stdenv.lib.optionalString crossCompiling
-      ''
-        mkdir -p $dev/lib/perl5/cross_perl/${version}
-        for dir in cnf/{stub,cpan}; do
-          cp -r $dir/* $dev/lib/perl5/cross_perl/${version}
-        done
-
-        mkdir -p $dev/bin
-        install -m755 miniperl $dev/bin/perl
-
-        export runtimeArch="$(ls $out/lib/perl5/site_perl/${version})"
-        # wrapProgram should use a runtime-native SHELL by default, but
-        # it actually uses a buildtime-native one. If we ever fix that,
-        # we'll need to fix this to use a buildtime-native one.
-        #
-        # Adding the arch-specific directory is morally incorrect, as
-        # miniperl can't load the native modules there. However, it can
-        # (and sometimes needs to) load and run some of the pure perl
-        # code there, so we add it anyway. When needed, stubs can be put
-        # into $dev/lib/perl5/cross_perl/${version}.
-        wrapProgram $dev/bin/perl --prefix PERL5LIB : \
-          "$dev/lib/perl5/cross_perl/${version}:$out/lib/perl5/${version}:$out/lib/perl5/${version}/$runtimeArch"
-      ''; # */
-  } // stdenv.lib.optionalAttrs (stdenv.buildPlatform != stdenv.hostPlatform) rec {
-    crossVersion = "ab8d05c9e695d3db4f7dc15c70f23623349c2f49"; # Oct 03, 2018
-
-    perl-cross-src = fetchurlBoot {
-      url = "https://github.com/arsv/perl-cross/archive/${crossVersion}.tar.gz";
-      sha256 = "1g7p7mqmx8x3diqvbh881gr72d106cn6yvm4gx7f0ars3n3b3wj0";
-    };
-
-    depsBuildBuild = [ buildPackages.stdenv.cc makeWrapper ];
-
-    postUnpack = ''
-      unpackFile ${perl-cross-src}
-      cp -R perl-cross-${crossVersion}/* perl-${version}/
-    '';
-
-    configurePlatforms = [ "build" "host" "target" ];
-
-    # TODO merge setup hooks
-    setupHook = ./setup-hook-cross.sh;
-  });
 in rec {
 
   # the latest Maint version
